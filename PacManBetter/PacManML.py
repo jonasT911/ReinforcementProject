e269fad1de19f09004563720a72a9dbe69cdeda3
jonas <jonastraweek@yahoo.com>
2023-04-14 17:51:08 -0500

    Made changes to start code

--- PacManBetter/PacManML.py
+++ PacManBetter/PacManML.py
@@ -1,11 +1,18 @@
 from MLfiles import agent
 import sys
-
+from run import GameController
 
 
 if __name__== '__main__':
 	print("Begin ML Pac-Man")
 	print ("Number of arguments:" + str(len(sys.argv)) + "arguments.")
 	print ("Argument List:"+ str(sys.argv))
-	agent.train()
-
+	if(sys.argv[1] == "m"):
+		agent.train()
+	elif (sys.argv[1] == "h"):
+		game = GameController()
+		game.startGame()
+		while True:
+			game.update()
+	else:
+		print("Arguments are h m and e")
